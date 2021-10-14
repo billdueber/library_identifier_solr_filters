2accaf01a29e2630f8fc401a4e0863791f25f1ab
Bill Dueber <bill@dueber.com>
2021-10-14 11:45:38 -0400

    CallnumberSortableFieldType now has separate stored and indexed values
    `CallnumberSortableFieldType` now finally has separate stored and
    indexed values, so the normalized version can be indexed and the
    passed value is stored. This should allow just using `callnumber`
    as a field and allow ranged and exact searches on it.
    
    Bumped version to 0.3 as well.

--- src/main/edu/umich/library/library_identifier/schema/CallnumberSortableFieldType.java
+++ src/main/edu/umich/library/library_identifier/schema/CallnumberSortableFieldType.java
@@ -1,11 +1,27 @@
 package edu.umich.library.library_identifier.schema;
 
 import edu.umich.library.library_identifier.normalizers.LCCallNumberSimple;
+import org.apache.lucene.document.SortedDocValuesField;
+import org.apache.lucene.document.SortedSetDocValuesField;
+import org.apache.lucene.index.IndexOptions;
+import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.search.SortField;
+import org.apache.lucene.util.BytesRef;
+import org.apache.solr.common.SolrException;
+import org.apache.solr.common.util.ByteArrayUtf8CharSequence;
+import org.apache.solr.response.TextResponseWriter;
+import org.apache.solr.schema.FieldType;
+import org.apache.solr.schema.SchemaField;
 import org.apache.solr.schema.StrField;
+import org.apache.solr.uninverting.UninvertingReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class CallnumberSortableFieldType extends StrField {
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
@@ -13,6 +29,25 @@ public class CallnumberSortableFieldType extends StrField {
   @Override
   public String toInternal(String val) {
     LCCallNumberSimple lccns = new LCCallNumberSimple(val);
-    return lccns.any_collation_key();
+    String ckey = lccns.any_collation_key();
+    return ckey;
+  }
+
+  @Override
+  public IndexableField createField(SchemaField field, Object value) {
+    if (!field.indexed() && !field.stored()) {
+      if (log.isTraceEnabled())
+        log.trace("Ignoring unindexed/unstored field: {}", field);
+      return null;
+    }
+
+    String val = value.toString();
+    if (val==null) return null;
+    org.apache.lucene.document.FieldType newType = new org.apache.lucene.document.FieldType();
+    newType.setTokenized(true);
+    newType.setStored(field.stored());
+    newType.setIndexOptions(IndexOptions.DOCS);
+
+    return createField(field.getName(), val, newType);
   }
 }
