f89faacaadfa8179ac479795d4464ee077e97679
Bill Dueber <bill@dueber.com>
2021-10-15 10:45:12 -0400

    Cleanup

--- src/main/edu/umich/library/library_identifier/schema/CallnumberSortableFieldType.java
+++ src/main/edu/umich/library/library_identifier/schema/CallnumberSortableFieldType.java
@@ -1,27 +1,14 @@
 package edu.umich.library.library_identifier.schema;
 
 import edu.umich.library.library_identifier.normalizers.LCCallNumberSimple;
-import org.apache.lucene.document.SortedDocValuesField;
-import org.apache.lucene.document.SortedSetDocValuesField;
 import org.apache.lucene.index.IndexOptions;
 import org.apache.lucene.index.IndexableField;
-import org.apache.lucene.search.SortField;
-import org.apache.lucene.util.BytesRef;
-import org.apache.solr.common.SolrException;
-import org.apache.solr.common.util.ByteArrayUtf8CharSequence;
-import org.apache.solr.response.TextResponseWriter;
-import org.apache.solr.schema.FieldType;
 import org.apache.solr.schema.SchemaField;
 import org.apache.solr.schema.StrField;
-import org.apache.solr.uninverting.UninvertingReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.lang.invoke.MethodHandles;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class CallnumberSortableFieldType extends StrField {
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
@@ -29,8 +16,7 @@ public class CallnumberSortableFieldType extends StrField {
   @Override
   public String toInternal(String val) {
     LCCallNumberSimple lccns = new LCCallNumberSimple(val);
-    String ckey = lccns.any_collation_key();
-    return ckey;
+    return lccns.any_collation_key();
   }
 
   @Override
