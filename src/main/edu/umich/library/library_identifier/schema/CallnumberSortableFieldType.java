987a135bda7bc99cdc168a84295a7b9d04cd0749
Bill Dueber <bill@dueber.com>
2021-10-19 22:25:49 -0400

    Allow CallnumberSortableFieldType to be strict or not, per config

--- src/main/edu/umich/library/library_identifier/schema/CallnumberSortableFieldType.java
+++ src/main/edu/umich/library/library_identifier/schema/CallnumberSortableFieldType.java
@@ -3,20 +3,36 @@ package edu.umich.library.library_identifier.schema;
 import edu.umich.library.library_identifier.normalizers.LCCallNumberSimple;
 import org.apache.lucene.index.IndexOptions;
 import org.apache.lucene.index.IndexableField;
+import org.apache.solr.schema.IndexSchema;
 import org.apache.solr.schema.SchemaField;
 import org.apache.solr.schema.StrField;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
+import java.util.Map;
+import java.util.Objects;
 
 public class CallnumberSortableFieldType extends StrField {
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  private Boolean passThroughInvalid = false;
+
+  protected void init(IndexSchema schema, Map<String, String> args) {
+    super.init(schema, args);
+    String p = args.remove("passThroughInvalid");
+    if (p != null) {
+      passThroughInvalid = Boolean.parseBoolean(p);
+    }
+  }
 
   @Override
   public String toInternal(String val) {
     LCCallNumberSimple lccns = new LCCallNumberSimple(val);
-    return lccns.collation_key();
+    if (passThroughInvalid) {
+      return lccns.any_collation_key();
+    } else {
+      return lccns.collation_key();
+    }
   }
 
   @Override
@@ -27,8 +43,14 @@ public class CallnumberSortableFieldType extends StrField {
       return null;
     }
 
+
     String val = value.toString();
-    if (val==null) return null;
+    if (val == null) return null;
+
+    if (!passThroughInvalid) {
+      if (!(new LCCallNumberSimple(val).isValid)) return null;
+    }
+
     org.apache.lucene.document.FieldType newType = new org.apache.lucene.document.FieldType();
     newType.setTokenized(true);
     newType.setStored(field.stored());
