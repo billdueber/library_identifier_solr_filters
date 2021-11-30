7e7a2e33cfd5a2e25aeac15cb1909903ee74b7cf
Bill Dueber <bill@dueber.com>
2021-11-30 15:43:25 -0500

    Move git st and || delimiters out of normalization code and into solr field code

--- src/main/edu/umich/library/library_identifier/schema/CallNumberSortKeyFieldType.java
+++ src/main/edu/umich/library/library_identifier/schema/CallNumberSortKeyFieldType.java
@@ -1,16 +1,12 @@
 package edu.umich.library.library_identifier.schema;
 
 import edu.umich.library.library_identifier.normalizers.LCCallNumberSimple;
-import org.apache.lucene.index.IndexOptions;
-import org.apache.lucene.index.IndexableField;
 import org.apache.solr.schema.IndexSchema;
-import org.apache.solr.schema.SchemaField;
 import org.apache.solr.schema.StrField;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
-import java.util.ArrayList;
 import java.util.Map;
 
 public class CallNumberSortKeyFieldType extends StrField  {
@@ -18,6 +14,7 @@ public class CallNumberSortKeyFieldType extends StrField  {
   private Boolean passThroughInvalid = false;
 
   private final String FIELD_DELIMITER = "\\|\\|";
+  private final String END_OF_CALLNUMBER = "!!";
 
   protected void init(IndexSchema schema, Map<String, String> args) {
     super.init(schema, args);
@@ -30,16 +27,16 @@ public class CallNumberSortKeyFieldType extends StrField  {
   @Override
   public String toInternal(String val) {
     String[] fields = val.split(FIELD_DELIMITER, 2);
-    String rest_of_fields = "";
+    String appended_fields = "";
     if (fields.length > 1) {
-      rest_of_fields = fields[1];
+      appended_fields = fields[1];
     }
 
     LCCallNumberSimple lccns = new LCCallNumberSimple(fields[0]);
     if (passThroughInvalid) {
-      return lccns.any_collation_key() +  rest_of_fields;
+      return lccns.any_collation_key() + END_OF_CALLNUMBER + FIELD_DELIMITER +  appended_fields;
     } else {
-      return lccns.collation_key()  + rest_of_fields;
+      return lccns.collation_key() + END_OF_CALLNUMBER  + FIELD_DELIMITER +  appended_fields;
     }
   }
 
